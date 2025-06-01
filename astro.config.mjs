3a89562029a5f3f04811f6361d5b4410c9e02c3e
artijo <a@artijo.com>
2025-06-01 08:56:07 +0700

    Add TailwindCSS with Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
